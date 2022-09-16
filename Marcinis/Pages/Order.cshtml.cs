2589229f4c49583fcac71d300297d275fe36e377
Kevin <kevinStefoni@gmail.com>
2022-09-16 07:26:41 -0500

    Transferred progress from RestaurantOnlineOrdering into Marcinis.

---
+++ Marcinis/Pages/Order.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Marcinis.Pages
+{
+    public class Order : PageModel
+    {
+        private readonly ILogger<Order> _logger;
+
+        public Order(ILogger<Order> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
