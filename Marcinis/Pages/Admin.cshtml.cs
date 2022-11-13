a982b8d112e4f68c7f780b3959514e6d0e083983
Brian Bissland <Brian.Bissland@gmail.com>
2022-11-13 01:45:41 -0600

    Functions Added on Admin page - Get, Delete, Add Customer/Users - Get, Delete, Add Menu Item - Get, Delete Orders - Get, Delete, Add Discount Codes All three Deletes have a bug that needs to be resolved. ID is not carrying over.

--- Marcinis/Pages/Admin.cshtml.cs
+++ Marcinis/Pages/Admin.cshtml.cs
@@ -17,15 +17,45 @@ namespace Marcinis.Pages
         public IList<Customer> customer { get; set; }
 
         [BindProperty]
+        public Customer cust { get; set; } //customer value for submission form on page
+
+        [BindProperty]
         public MenuItem item { get; set; }
+
+        [BindProperty]
+        public DiscountCode disc { get; set; } //discount value for submission form on page
+
+        [BindProperty]
+        public IList<MenuItem> menu { get; set; }
+
+        [BindProperty]
+        public IList<CustomerOrder> orders { get; set; }
+
+        [BindProperty]
+        public IList<DiscountCode> discounts { get; set; }
 
         [BindProperty]
         public int id { get; set; }
 
+        [BindProperty]
+        public int itemId { get; set; }
+
+        [BindProperty]
+        public int orderId { get; set; }
+
+        [BindProperty]
+        public int discountId { get; set; }
+
         public void OnGet()
         {
             customer = customerRepo.GetAllCustomers();
+            menu = itemRepo.GetMenu();
+            orders = itemRepo.GetOrders();
+            discounts = itemRepo.GetDiscountCodes();
             SessionHelper.SetObjectAsJson(HttpContext.Session, "customers", customer);
+            SessionHelper.SetObjectAsJson(HttpContext.Session, "menu", menu);
+            SessionHelper.SetObjectAsJson(HttpContext.Session, "orders", orders);
+            SessionHelper.SetObjectAsJson(HttpContext.Session, "discounts", discounts);
         }
 
         public ActionResult OnPost()
@@ -49,11 +79,46 @@ namespace Marcinis.Pages
             return Redirect("./Admin");
         }
 
+        public ActionResult OnPostAddCustomer()
+        {
+            customerRepo.AddCustomer(cust);
+
+            return Redirect("./Admin");
+        }
+
+        public ActionResult OnPostAddDiscountCode()
+        {
+            itemRepo.AddDiscountCode(disc);
+
+            return Redirect("./Admin");
+        }
+
         public void OnPostCustomerDelete()
         {
             customerRepo.DeleteCustomer(id);
             customer = customerRepo.GetAllCustomers();
             SessionHelper.SetObjectAsJson(HttpContext.Session, "customers", customer);
+        }
+
+        public void OnPostMenuItemDelete()
+        {
+            itemRepo.DeleteMenuItem(itemId);
+            menu = itemRepo.GetMenu();
+            SessionHelper.SetObjectAsJson(HttpContext.Session, "menu", menu);
+        }
+
+        public void OnPostOrderDelete()
+        {
+            itemRepo.DeleteMenuItem(orderId);
+            orders = itemRepo.GetOrders();
+            SessionHelper.SetObjectAsJson(HttpContext.Session, "orders", orders);
+        }
+
+        public void OnPostDiscountCodeDelete()
+        {
+            itemRepo.DeleteDiscountCode(discountId);
+            discounts = itemRepo.GetDiscountCodes();
+            SessionHelper.SetObjectAsJson(HttpContext.Session, "discounts", discounts);
         }
     }
 }
